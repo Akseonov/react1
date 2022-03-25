993df12f1d6c6efb5ddfba8645cd3704e199e777
Akseonov <konstantinaxeonov@gmail.com>
2022-03-25 16:43:58 +0500

    lesson-3-class  - Completed homework for lesson 3 on class components

--- src/react/App.js
+++ src/react/App.js
@@ -1,4 +1,22 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListSubheader from '@mui/material/ListSubheader';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+    },
+});
 
 class App extends React.Component {
     constructor(props) {
@@ -6,10 +24,31 @@ class App extends React.Component {
 
         this.state = {
             messageList: [],
+            element: React.createRef(),
+            chatList: [
+                {
+                    id: 1,
+                    name: "Друзья",
+                },
+                {
+                    id: 2,
+                    name: "Семья",
+                },
+                {
+                    id: 3,
+                    name: "Работа",
+                },
+            ]
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.botAnswer = this.botAnswer.bind(this);
+    }
+
+    focusTextField(input) {
+        if (input) {
+            input.focus();
+        }
     }
 
     handleSubmit(event) {
@@ -35,6 +74,7 @@ class App extends React.Component {
 
     componentDidUpdate() {
         setTimeout( () => {
+            this.focusTextField(this.state.element.current);
             this.botAnswer(this.state.messageList);
         }, 1500 );
     }
@@ -55,27 +95,57 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="container">
-                    <form onSubmit={this.handleSubmit} className="form">
-                        <label className="form__label">
-                            <span className="form__span">Имя:</span>
-                            <input className="form__input" type="text" name="author"/>
-                        </label>
-                        <label className="form__label">
-                            <span className="form__span">Сообщение:</span>
-                            <input className="form__input" type="textarea" name="text"/>
-                        </label>
-                        <input className="form__button" type="submit" value="Отправить"/>
-                    </form>
-                    <div className="message-list">
-                        {this.state.messageList.map(message => <div className="message-list__item" key={message.id}>
-                            {message.author && <p className="message-list__p"><span>Автор:</span> {message.author}</p>}
-                            <p className="message-list__p">{message.author && <span>Текст:</span>} {message.text}</p>
-                        </div>)}
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <div style={{
+                        position: 'absolute',
+                        left: 0,
+                        top: 0, }}>
+                        <List subheader={<ListSubheader>Чаты</ListSubheader>}
+                              sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                            {
+                                this.state.chatList.map( chat => <ListItem key={chat.id}>
+                                    <ListItemButton>
+                                        <ListItemText primary={chat.name} />
+                                    </ListItemButton>
+                                </ListItem> )
+                            }
+                        </List>
+                    </div>
+                    <div className="container">
+                        <Box component="form" noValidate
+                             autoComplete="off" mt={4} display="flex"
+                             flexDirection="column" onSubmit={this.handleSubmit}>
+                            <TextField id="name" label="Имя"
+                                       variant="outlined" sx={{ mb: 2 }}
+                                       name="author" autoFocus
+                                       inputRef={this.state.element} />
+                            <TextField id="message" label="Сообщение"
+                                       variant="outlined" sx={{ mb: 2 }}
+                                       name="text" />
+                            <Button variant="outlined" color="primary"
+                                    size="small" type="submit" sx={{ mb: 4 }}>Отправить</Button>
+                        </Box>
+                        {/*<form onSubmit={this.handleSubmit} className="form">*/}
+                        {/*    <label className="form__label">*/}
+                        {/*        <span className="form__span">Имя:</span>*/}
+                        {/*        <input className="form__input" type="text" name="author"/>*/}
+                        {/*    </label>*/}
+                        {/*    <label className="form__label">*/}
+                        {/*        <span className="form__span">Сообщение:</span>*/}
+                        {/*        <input className="form__input" type="textarea" name="text"/>*/}
+                        {/*    </label>*/}
+                        {/*    <input className="form__button" type="submit" value="Отправить"/>*/}
+                        {/*</form>*/}
+                        <div className="message-list">
+                            {this.state.messageList.map(message => <div className="message-list__item" key={message.id}>
+                                {message.author && <p className="message-list__p"><span>Автор:</span> {message.author}</p>}
+                                <p className="message-list__p">{message.author && <span>Текст:</span>} {message.text}</p>
+                            </div>)}
+                        </div>
                     </div>
                 </div>
-            </div>
+            </ThemeProvider>
         );
     }
 }
